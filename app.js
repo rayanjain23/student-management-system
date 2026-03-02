87a0c764051ceaeafb01547cb8706e5c0eb25464
Dev A - Rayan <rayan.jain23@spit.ac.in>
2026-03-02 16:17:07 +0530

    updated logic for feature-auth

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 function login(user) {
- if (user.role === "student") {
-  return "Student Dashboard";
- }
- if (user.role === "admin") {
-  return "Admin Dashboard";
- }
- return "Unauthorized";
+  if (!user) {
+    return "No User";
+  }
+
+  if (!user.isActive) {
+    return "Account Inactive";
+  }
+
+  if (user.role === "student") {
+    return "Student Dashboard";
+  }
+
+  if (user.role === "admin") {
+    return "Admin Dashboard";
+  }
+
+  return "Unauthorized";
 }
+
 module.exports = login;
