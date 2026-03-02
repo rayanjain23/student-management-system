9a71a0b5b21bce10c33ae15aeaf52a69d3a2d915
Dev A - Rayan <rayan.jain23@spit.ac.in>
2026-03-02 16:16:04 +0530

    updated logic for main

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 function login(user) {
- if (user.role === "student") {
-  return "Welcome Student";
- }
- return "Access Denied";
+  if (!user) {
+    return "No User";
+  }
+
+  if (user.role === "student") {
+    return "Student Logged In";
+  }
+
+  return "Login Failed";
 }
+
 module.exports = login;
